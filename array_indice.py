6b23293ead6b80f45a23073e835c891d37e52d21
EmilioBorges <emilio.borges2012@gmail.com>
2024-04-04 20:26:43 -0300

    Questao 2

--- array_indice.py
+++ array_indice.py
@@ -1,6 +1,14 @@
-def imprimir_nomes():
-    nome = ['João', "Maria","Fulano","Ana"]
+# def imprimir_nomes():
+#     nome = ['João', "Maria","Fulano","Ana"]
 
-    print(f'1 - {nome[0]}\n2 - {nome[1]}\n3 - {nome[2]}\n4 - {nome[3]}')
+#     print(f'1 - {nome[0]}\n2 - {nome[1]}\n3 - {nome[2]}\n4 - {nome[3]}')
 
-imprimir_nomes()
+# imprimir_nomes()
+
+
+def primeiro_ultimo_nome():
+    nome = ['Emilio','Ana','Thor','Bia','Juliana']
+
+    print(f'1 - {nome[0]}\n4 - {nome[4]}')
+
+primeiro_ultimo_nome()
