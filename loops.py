eae47cf22978bb31e8f370eeada531fd0dd2ca07
EmilioBorges <emilio.borges2012@gmail.com>
2024-04-04 21:38:36 -0300

    Questao 7 looop

--- loops.py
+++ loops.py
@@ -4,12 +4,19 @@
 
 # conta_ate_20()
 
-def limitar_contador(valor_usuario):
-    num = 0
-    while num <= valor_usuario:
-        print(num)
-        num+= 1
+# def limitar_contador(valor_usuario):
+#     num = 0
+#     while num <= valor_usuario:
+#         print(num)
+#         num+= 1
 
-num_do_usuario = int(input("Digite o numero final: "))
+# num_do_usuario = int(input("Digite o numero final: "))
 
-limitar_contador(num_do_usuario)
+# limitar_contador(num_do_usuario)
+
+def tabuada_2():
+    for i in range(0,11):
+        print(f'{i} + {2} = {i+2}')
+
+
+tabuada_2()
