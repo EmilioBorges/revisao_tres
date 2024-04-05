28041d2e374d0a8ca08a9b5d25f0743796958add
EmilioBorges <emilio.borges2012@gmail.com>
2024-04-04 21:33:34 -0300

    Questao 6 looop

--- loops.py
+++ loops.py
@@ -1,6 +1,15 @@
-def conta_ate_20():
-    for i in range(1, 21):
-        print(i)
+# def conta_ate_20():
+#     for i in range(1, 21):
+#         print(i)
 
-conta_ate_20()
+# conta_ate_20()
 
+def limitar_contador(valor_usuario):
+    num = 0
+    while num <= valor_usuario:
+        print(num)
+        num+= 1
+
+num_do_usuario = int(input("Digite o numero final: "))
+
+limitar_contador(num_do_usuario)
